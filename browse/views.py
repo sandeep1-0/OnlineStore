bbfce4c0ece1dfdc956dbdca354d85d84ea541ea
sandeep1-0 <sandeepp163@gmail.com>
2019-08-02 23:10:40 +0530

    cahnges in views

--- browse/views.py
+++ browse/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def Home_page(request):
     return render(request,'home_page.html')
 
 
+@csrf_exempt
 def webhook_notification(request):
-    print(request.GET,111111)
-    return HttpResponse({"test": "test"})
+    print(request.POST,111111)
+    return HttpResponse(status=200)
