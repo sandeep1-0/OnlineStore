be4bcf7027d14fa3fec450a3d69c88d5fb4078e3
sandeep1-0 <sandeepp163@gmail.com>
2019-08-03 13:00:23 +0530

    models added

--- browse/admin.py
+++ browse/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from browse.models import *
 
-# Register your models here.
+
+class OrderDetailsAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderItemDetailsAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderPriceDetailsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(OrderDetails, OrderDetailsAdmin)
+admin.site.register(OrderItemDetails, OrderItemDetailsAdmin)
+admin.site.register(OrderPriceDetails, OrderPriceDetailsAdmin)
--- browse/models.py
+++ browse/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class OrderDetails(models.Model):
+    order_id = models.IntegerField(primary_key=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=14, null=True, blank=True)
+    notes = models.CharField(max_length=200, null=True, blank=True)
+
+
+class OrderPriceDetails(models.Model):
+    order = models.ForeignKey(OrderDetails, on_delete=models.CASCADE)
+    subtotal_price = models.IntegerField()
+    total_price = models.IntegerField()
+    total_tax = models.IntegerField()
+
+
+class OrderItemDetails(models.Model):
+    order = models.ForeignKey(OrderDetails, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    quantity = models.IntegerField()
+    fulfillment_status = models.CharField(max_length=100, null=True, blank=True)
