f197f5d5fdaa30c04c133bcd6d28cdf6e0dedde0
sandeep1-0 <sandeepp163@gmail.com>
2019-08-04 18:22:31 +0530

    fixes

--- browse/forms.py
+++ browse/forms.py
@@ -3,7 +3,7 @@ from browse.models import OrderDetails
 
 
 class UpdateForm(forms.ModelForm):
-    order_id = forms.CharField(disabled=True)
+    order_id = forms.CharField(max_length=14, widget=forms.TextInput(attrs={'readonly':'readonly'}))
     notes = forms.CharField(disabled=True)
 
     class Meta:
